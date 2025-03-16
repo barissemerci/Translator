961e454ca1ae0931ad4c0d82b11ee36ebbe49125
Barış SEMERCİ <barissemerci3@gmail.com>
2025-03-16 17:09:24 +0300

    add colors and Android Theme

--- androidApp/src/main/java/com/example/translator/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/translator/android/MainActivity.kt
@@ -3,39 +3,103 @@ package com.example.translator.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.translator.Greeting
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+fun TranslatorTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        com.example.translator.android.core.theme.darkColors
+    } else {
+        com.example.translator.android.core.theme.lightColors
+    }
+    val SfProText = FontFamily(
+        Font(
+            resId = R.font.sf_pro_text_regular,
+            weight = FontWeight.Normal
+        ),
+        Font(
+            resId = R.font.sf_pro_text_medium,
+            weight = FontWeight.Medium
+        ),
+        Font(
+            resId = R.font.sf_pro_text_bold,
+            weight = FontWeight.Bold
+        ),
+    )
+    val typography = Typography(
+        h1 = TextStyle(
+            fontFamily = SfProText,
+            fontWeight = FontWeight.Bold,
+            fontSize = 30.sp
+        ),
+        h2 = TextStyle(
+            fontFamily = SfProText,
+            fontWeight = FontWeight.Bold,
+            fontSize = 24.sp
+        ),
+        h3 = TextStyle(
+            fontFamily = SfProText,
+            fontWeight = FontWeight.Medium,
+            fontSize = 18.sp
+        ),
+        body1 = TextStyle(
+            fontFamily = SfProText,
+            fontWeight = FontWeight.Normal,
+            fontSize = 14.sp
+        ),
+        body2 = TextStyle(
+            fontFamily = SfProText,
+            fontWeight = FontWeight.Normal,
+            fontSize = 12.sp
+        ),
+    )
+    val shapes = Shapes(
+        small = RoundedCornerShape(4.dp),
+        medium = RoundedCornerShape(4.dp),
+        large = RoundedCornerShape(0.dp)
+    )
+
+    MaterialTheme(
+        colors = colors,
+        typography = typography,
+        shapes = shapes,
+        content = content
+    )
+}
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Text("Hello, Android!")
-          /*  MyApplicationTheme {
+            TranslatorTheme {
+                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    GreetingView(Greeting().greet())
+
                 }
-            }*/
+            }
         }
     }
 }
 
-@Composable
-fun GreetingView(text: String) {
-    Text(text = text)
-}
 
-@Preview
-@Composable
-fun DefaultPreview() {
-  //  MyApplicationTheme {
-    //    GreetingView("Hello, Android!")
-   // }
-}
+
