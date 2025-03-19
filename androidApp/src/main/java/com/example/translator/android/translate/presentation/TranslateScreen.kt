2dc5032e7df03efa8151ade0f8373c286f400965
Barış SEMERCİ <barissemerci3@gmail.com>
2025-03-19 15:48:44 +0300

    add voice to text screen and arguments

--- androidApp/src/main/java/com/example/translator/android/translate/presentation/TranslateScreen.kt
+++ androidApp/src/main/java/com/example/translator/android/translate/presentation/TranslateScreen.kt
@@ -8,8 +8,12 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.FabPosition
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
@@ -17,9 +21,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalClipboardManager
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.unit.dp
 import com.example.translator.android.R
@@ -57,6 +64,24 @@ fun TranslateScreen(
         }
     }
     Scaffold(
+        floatingActionButtonPosition = FabPosition.Center,
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { onEvent(TranslateEvent.RecordAudio) },
+                backgroundColor = MaterialTheme.colors.primary,
+                contentColor = MaterialTheme.colors.onPrimary,
+                modifier = Modifier
+                    .size(75.dp)
+            ) {
+               Icon(
+                   imageVector = ImageVector.vectorResource(
+                       R.drawable.mic
+                   ),
+                   contentDescription = stringResource(R.string.record_audio)
+               )
+            }
+
+        }
     ) { innerPadding ->
         LazyColumn(
             modifier = modifier
